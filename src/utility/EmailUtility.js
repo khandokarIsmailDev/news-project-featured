fe2ea0203a5dd0178c4f8e6fd74d4426041aaa02
khandokarIsmailDev <ismailkhandokar000@gmail.com>
2024-08-16 01:53:10 +0600

    email setup for recovery otp

---
+++ src/utility/EmailUtility.js
@@ -0,0 +1,27 @@
+import nodemailer from 'nodemailer'
+
+
+export async function SendEmail(EmailTo,EmailText,EmailSubject){
+
+    let Transport = nodemailer.createTransport({
+        host:"mail.teamrabbil.com",
+        port:25,
+        auth:{
+            user: "info@teamrabbil.com",
+            pass: '~sR4[bhaC[Qs'
+        },
+        tls:{
+            rejectUnauthorized:false
+        }
+    })
+
+    let MailOption = {
+        from:'Next Js News Portal <info@teamrabbil.com',
+        to:EmailTo,
+        subject:EmailSubject,
+        text:EmailText
+    }
+
+    return await Transport.sendMail(MailOption)
+
+}
