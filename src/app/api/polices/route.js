8f8bbfaeb6d827a804886ea13f8d217b13825cf3
khandokarIsmailDev <ismailkhandokar000@gmail.com>
2024-08-18 01:21:42 +0600

    polices api done

---
+++ src/app/api/polices/route.js
@@ -0,0 +1,17 @@
+import { PrismaClient } from "@prisma/client";
+import { NextResponse } from "next/server";
+
+export async function GET(req,res){
+    try{
+        let prisma = new PrismaClient()
+        let {searchParams} = new URL(req.url)
+        let type = searchParams.get('type')
+        let result = await prisma.policies.findUnique({
+            where:{type:type}
+        })
+
+        return NextResponse.json({status:"success",data:'got data'})
+    }catch(err){
+        return NextResponse.json({status:"fail",data:err.toString()})
+    }
+}
