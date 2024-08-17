5949edd96691a4a1cae505a6debaaa1db4cffc17
khandokarIsmailDev <ismailkhandokar000@gmail.com>
2024-08-17 23:02:23 +0600

    usre profile update done

---
+++ src/app/api/user/profile/update/route.js
@@ -0,0 +1,22 @@
+import { PrismaClient } from "@prisma/client";
+import { headers } from "next/headers";
+import { NextResponse } from "next/server";
+
+export async function POST(req,res){
+    try{
+        let headerList = headers()
+        let id = parseInt(headerList.get('id'))
+        let reqBody = await req.json()
+        const prisma = new PrismaClient()
+
+        let result = await prisma.users.update({
+            where:{id:id},
+            data:reqBody
+        })
+
+        return NextResponse.json({status:"success",data:result})
+
+    }catch(err){
+        return NextResponse.json({status:"success",data:err})
+    }
+}
