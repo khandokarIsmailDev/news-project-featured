cf311f162ea164d44afbdbc2c85afc4ee8cc88dc
khandokarIsmailDev <ismailkhandokar000@gmail.com>
2024-08-18 14:49:21 +0600

    news search by keywords api done

---
+++ src/app/api/news/search/route.js
@@ -0,0 +1,17 @@
+import { PrismaClient } from "@prisma/client";
+import { NextResponse } from "next/server";
+
+export async function GET(req){
+    try{
+        let {searchParams}= new URL(req.url)
+        let keywords = searchParams.get('keywords')
+        let prisma = new PrismaClient()
+        let result = await prisma.news_list.findMany({
+            where:{title:{contains:keywords}}
+        })
+
+        return NextResponse.json({status:"success",data:''})
+    }catch(err){
+        return NextResponse.json({status:"fail",data:err})
+    }
+}
